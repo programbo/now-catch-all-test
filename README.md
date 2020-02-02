703ee2ff4d0d251a38ba76f511042b52fffe1c9c
John Lombardo <john@programbo.com>
2020-02-02 12:34:24 +0800

    Convert this test tp Typescript

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # now-catch-all-test
+
+This site is using `[...slug].tsx` as the catch-all route.
+
+In local development, visiting `/` should show **The slug is: home!**
+
+Visit [https://catch-all-test-mrjohn.meerkats.now.sh/](https://catch-all-test-mrjohn.meerkats.now.sh/) (deployed to Now) shows **The slug is: !**
+
+Visiting `/chicken/wings` on either shows **The slug is: chicken/wings!**
---
+++ pages/[...slug].tsx
@@ -0,0 +1,50 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Home = ({ slug }) => (
+  <div>
+    <Head>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <div className="hero">
+      <h1 className="title">
+        The slug is: <code>{slug}</code>!
+      </h1>
+      <p className="description">
+        The rewrite rule in <code>now.json</code> redirects <code>/</code> to{' '}
+        <code>/home</code>.
+      </p>
+    </div>
+
+    <style jsx>{`
+      .hero {
+        width: 100%;
+        color: #333;
+      }
+      .title {
+        margin: 0;
+        width: 100%;
+        padding-top: 80px;
+        line-height: 1.15;
+        font-size: 48px;
+      }
+      .title,
+      .description {
+        text-align: center;
+      }
+    `}</style>
+  </div>
+)
+
+Home.getInitialProps = ({ query }) => {
+  const slug = query.slug
+  console.log({ slug })
+
+  return {
+    slug: Array.isArray(slug) ? slug.join('/') : slug,
+  }
+}
+
+export default Home
